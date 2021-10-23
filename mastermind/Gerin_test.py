53fece36cb1603155996bb1303736a43bd9f8b0e
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-10-23 15:54:03 -0500

    Update Gerin_test.py

--- mastermind/Gerin_test.py
+++ mastermind/Gerin_test.py
@@ -4,23 +4,25 @@ origonal = ["*","*","*","*"]
 guess = [1, 2, 3, 4]
 numbers = []
 for i in range(4):#give 4 random numbers, from 1-20
-            numbers.append(random.randint(0,9))
+            numbers.append(random.randint(0, 9))
 
 print(f"{numbers[0]} {numbers[1]} {numbers[2]} {numbers[3]}")
 for i in numbers:
     for n in guess:
         if i == n:
             origonal[numbers.index(i)] = "O"
-        print(origonal)
+        #print(origonal)
             
 
             
 
 
 
-        print(f"{i}{n}") 
-new = []
-word = 1245
-new.append(word[1])
+        #print(f"{i}{n}") 
 
-print(new)
+print(origonal)
+# new = []
+# word = 1245
+# new = list(str(word))
+
+# print(new)

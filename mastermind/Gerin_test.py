4a0cf873b14e5cb14fa4f56477902c50c7ea84f7
GerinWilde1 <91751688+GerinWilde1@users.noreply.github.com>
2021-10-23 16:33:39 -0500

    Update Gerin_test.py

--- mastermind/Gerin_test.py
+++ mastermind/Gerin_test.py
@@ -2,25 +2,47 @@ import random
 origonal = ["*","*","*","*"]
         # """sets up the number to be guessed and places it in the list _piles"""
 guess = [1, 2, 3, 4]
-numbers = []
-for i in range(4):#give 4 random numbers, from 1-20
-            numbers.append(random.randint(0, 9))
+numbers = [1, 3, 2, 4]
+# for i in range(4):#give 4 random numbers, from 1-20
+#             numbers.append(random.randint(0, 9))
 
-print(f"{numbers[0]} {numbers[1]} {numbers[2]} {numbers[3]}")
+# print(f"{numbers[0]} {numbers[1]} {numbers[2]} {numbers[3]}")
+
+
+
+
+
+
+
+
+
 for i in numbers:
     for n in guess:
-        if i == n:
+        if i == n and guess.index(n) != numbers.index(i):
+            origonal[guess.index(n)] = "X"
+        elif i == n:
             origonal[numbers.index(n)] = "O"
+
         
-            
 
-            
+# print(origonal)
 
+# index = guess.index(numbers)
+# print(index)
 
-
-        #print(f"{i}{n}") 
-
+# origonal[index] = "X"
 print(origonal)
+
+
+
+
+
+
+
+
+
+
+
 # new = []
 # word = 1245
 # new = list(str(word))
